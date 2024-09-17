103ffe2903fa75b8e8380b707f1b55e00d822788
Anatoliy Sysoienko <pistalierro@gmail.com>
2024-09-17 13:04:25 +0300

    разметка и оформление блока main-navbar

---
+++ src/app/markup/main/main-navbar/main-navbar.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+
+@Component({
+  selector: 'app-main-navbar',
+  standalone: true,
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './main-navbar.component.html',
+  styleUrl: './main-navbar.component.scss'
+})
+export class MainNavbarComponent {
+
+}
