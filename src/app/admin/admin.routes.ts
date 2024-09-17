1b8e2940b79dad51b75580f35af974425ce4f7c1
Anatoliy Sysoienko <pistalierro@gmail.com>
2024-09-17 20:51:10 +0300

    сделал 2 компонента editPhrases, editPhrase, оформил, настроил редактирование

--- src/app/admin/admin.routes.ts
+++ src/app/admin/admin.routes.ts
@@ -1,9 +1,10 @@
 import {Routes} from '@angular/router';
 import {AdminHostComponent} from './admin-host/admin-host.component';
-import {ManagePhrasesComponent} from './manage-phrases/manage-phrases.component';
+import {canActivateGuard} from '../shared/can-activate.guard';
+import {EditPhrasesListComponent} from '../edit-phrases/edit-phrases-list/edit-phrases-list.component';
 import {UsersListComponent} from './users-list/users-list.component';
 import {UserDetailsComponent} from './user-details/user-details.component';
-import {canActivateGuard} from '../shared/can-activate.guard';
+import {EditPhraseDetailsComponent} from '../edit-phrases/edit-phrase-details/edit-phrase-details.component';
 
 export const adminRoutes: Routes = [
   {
@@ -11,11 +12,15 @@ export const adminRoutes: Routes = [
     component: AdminHostComponent,
     canActivate: [canActivateGuard],
     children: [
-      {path: 'phrases', component: ManagePhrasesComponent},
-      {path: '', component: ManagePhrasesComponent},
+      {path: '', redirectTo: 'phrases', pathMatch: 'full'},
       {
-        path: 'users',
-        component: UsersListComponent,
+        path: 'phrases', component: EditPhrasesListComponent,
+        children: [
+          {path: ':id', component: EditPhraseDetailsComponent},
+        ]
+      },
+      {
+        path: 'users', component: UsersListComponent,
         children: [
           {path: ':id', component: UserDetailsComponent},
         ]
