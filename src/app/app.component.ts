bc1f459f366f6fb418a1225b46eb7c6f23938df3
Anatoliy Sysoienko <pistalierro@gmail.com>
2024-09-17 11:38:58 +0300

    подключил все файлы scss, добавил шрифты

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HeaderComponent} from './markup/header/header.component';
+import {MainComponent} from './markup/main/main.component';
+import {FooterComponent} from './markup/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, MainComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
